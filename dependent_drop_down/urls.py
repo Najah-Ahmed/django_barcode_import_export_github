ad037800af60b390e6a73eb79addf5c1b664908d
najah said <najaah8800@gmail.com>
2021-01-01 13:13:49 +0300

    adding depends dropdown using ajax minor change

--- dependent_drop_down/urls.py
+++ dependent_drop_down/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.item_create_view, name='add_item'),
+    path('add/', views.item_create_view, name='add_item'),
     path('edit/<int:pk>/', views.item_update_view, name='change_item'),
     path('ajax/sub_cat/', views.load_sub_cat, name='sub_cat'),  # AJAX
 ]
