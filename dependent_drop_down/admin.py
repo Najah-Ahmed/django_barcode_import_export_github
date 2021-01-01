446a6e442ee22ce715270331eea103d56ec42b27
najah said <najaah8800@gmail.com>
2021-01-01 13:41:18 +0300

    adding export and import in dependent_drop_down

--- dependent_drop_down/admin.py
+++ dependent_drop_down/admin.py
@@ -1,6 +1,21 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
-
+from import_export import resources
 from .models import ItemModel, Category, Sub_Category
 
 MyModels = [ItemModel, Category, Sub_Category]
-admin.site.register(MyModels)
+
+
+class ItemResource(resources.ModelResource):
+    class Meta:
+        model = ItemModel
+        skip_unchange = True
+        report_skipped = False
+        exclude = ('barcode', )
+
+
+class ItemAdmin(ImportExportModelAdmin):
+    resource_class = ItemResource
+
+
+admin.site.register(MyModels, ItemAdmin)
